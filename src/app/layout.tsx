9703d50293f2ffacb1ccf1ab1c233d9fc2a58c80
Aniket Raj <aniketronaldo10@gmail.com>
2025-06-27 15:51:03 +0530

    Added favicon and other metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,21 @@ const spaceGrotesk = Space_Grotesk({
 
 export const metadata = {
   title: 'Aniket Raj',
-  description: 'Personal Website of Aniket Raj',
+  description:
+    'Mostly friendly. Sometimes nerdy. Always curious. Currently doing a bit of everything at Devfolio and trying not to break anything important.',
+  icons: {
+    icon: '/favicon.ico', // Update path if your favicon is different
+  },
+  openGraph: {
+    images: [
+      {
+        url: '/aniket-raj-profile.png', // Update path to your OG image
+        width: 1200,
+        height: 630,
+        alt: 'Aniket Raj',
+      },
+    ],
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
