cb0483a9ce7d503f74e74d7599adb46f8fd6e104
Aniket Raj <aniketronaldo10@gmail.com>
2025-07-07 19:29:06 +0530

    Undid all braindump -_- to upgrade to a better system

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
-import { withContentlayer } from 'next-contentlayer'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   // Add any other config options you use
 }
 
-export default withContentlayer(nextConfig)
+export default nextConfig
