b92263b771c09c61002741217b0436d6c78ad9c8
Aniket Raj <aniketronaldo10@gmail.com>
2025-07-10 14:48:53 +0530

    Removed experimental setting

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ import withMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
   // Add any other config options you use
 }
 
