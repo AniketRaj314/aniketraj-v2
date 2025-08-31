2b8bd0c8b5a810481d46e500de95311f76dcd860
Aniket <aniketronaldo10@gmail.com>
2025-08-31 18:26:41 +0530

    Added redirection

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,20 @@ import withMDX from '@next/mdx'
 const nextConfig = {
   reactStrictMode: true,
   // Add any other config options you use
+  async redirects() {
+    return [
+      {
+        source: '/blr-house',
+        destination: 'https://maps.app.goo.gl/PMVnqYE9hnPa6ygc9',
+        permanent: true,
+      },
+      {
+        source: '/bom-house',
+        destination: 'https://maps.app.goo.gl/6LCukt6DkcTpV6Ec7',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 export default withMDX()(nextConfig)
